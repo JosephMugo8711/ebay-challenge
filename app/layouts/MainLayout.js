29aae1ac99e477b8abc54fabb4f77dff4d3fbff2
JosephMugo8711 <wamiti8711@gmail.com>
2024-03-30 23:38:49 +0300

    Feat: TopMenu

--- app/layouts/MainLayout.js
+++ app/layouts/MainLayout.js
@@ -1,10 +1,12 @@
 "use client"
 
+import TopMenu from "./includes/TopMenu"
+
 export default function MainLayout({ children }) {
     return (
         <>
-           <div id="Mainlayout">
-            ML
+           <div id="Mainlayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
+              <TopMenu />
 
            </div>
 
