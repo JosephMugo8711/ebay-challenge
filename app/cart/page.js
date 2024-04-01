83402a2104442fa2ad34d7cb58a4ddadc8ccd40a
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-01 17:05:04 +0300

    Feat: cart page in progress

---
+++ app/cart/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout"
+import SimilarProducts from "../components/SimilarProducts"
+
+export default function Cart() {
+    return(
+        <>
+           <MainLayout>
+              <SimilarProducts />
+           </MainLayout>
+        </>
+    )
+}
