d2393a8c7c06a228dc5a9665ff26b71db91930ce
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-05 16:53:17 +0300

    Feature: Auth page in progress

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,17 +1,17 @@
-// "use client"
+"use client"
 
-// import {Auth} from "@supabase/auth-ui-react"
-// import {ThemeSupa} from "@supabase/auth-ui-shared"
-// import Link from 'next/link'
+import {Auth} from "@supabase/auth-ui-react"
+import {ThemeSupa} from "@supabase/auth-ui-shared"
+import Link from 'next/link'
 
-// export default function AuthPage() {
-//     return (
-//         <>
-//            <div id="AuthPage" className="w-full min-h-screen bg-white">
+export default function AuthPage() {
+    return (
+        <>
+           <div id="AuthPage" className="w-full min-h-screen bg-white">
 
               
 
-//            </div>
-//         </>
-//     )
-// }
+           </div>
+        </>
+    )
+}
