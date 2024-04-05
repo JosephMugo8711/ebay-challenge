5797c6bea5db5e0e417d9dd4e8a8c8d80be01c7e
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-05 10:00:37 +0300

    Feat: orders page in progress

---
+++ app/orders/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link"
+import {CiDeliveryTruck} from "react-icons/ci";
+import MainLayout from "../layouts/MainLayout";
+
+export default function Orders() {
+    return (
+        <>
+           <MainLayout>
+            
+           </MainLayout>
+        </>
+    )
+}
