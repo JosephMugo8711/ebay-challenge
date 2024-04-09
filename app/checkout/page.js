669bb7e2646b8552a8cfbbd0b1b4ef66505c3c7f
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-09 22:42:34 +0300

    Feat: Stripe Setup and Finished the checkout page

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,88 +1,257 @@
 "use client"
-
-import MainLayout from "../layouts/MainLayout"
-import CheckoutItem from "../components/CheckoutItem"
+import CheckoutItem from "../components/CheckoutItem";
+import MainLayout from "../layouts/MainLayout";
+import Link from "next/link";
+import { useUser } from "@/app/context/user";
+import { useCart } from "../context/cart";
+import { useEffect, useRef, useState } from "react";
+import useIsLoading from "../hooks/useIsLoading";
+import useUserAddress from "../hooks/useUserAddress";
+import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import ClientOnly from "../components/ClientOnly";
+import { loadStripe } from '@stripe/stripe-js';
 
 export default function Checkout() {
+    // Get user and cart data
+    const user = useUser();
+    const cart = useCart();
+    const router = useRouter();
 
-    const product = {
-        id: 2,
-        title: "School Books",
-        description: "The school book is a guide for students, offering clear explanations and information on various subjects to support their learning.",
-        url: "https://picsum.photos/id/20",
-        price: 1999
-    }
+    // References for Stripe and elements
+    let stripe = useRef(null);
+    let elements = useRef(null);
+    let card = useRef(null);
+    let clientSecret = useRef(null);
 
-    return(
+    // State for address details and loading state
+    const [addressDetails, setAddressDetails] = useState({});
+    const [isLoadingAddress, setIsLoadingAddress] = useState(false);
+
+    // Load user's address details and initialize Stripe
+    useEffect(() => {
+        // If cart is empty, show error and redirect to homepage
+        if (cart?.cartTotal() <= 0) {
+            toast.error("Your cart is empty!", { autoClose: 3000 });
+            return router.push('/');
+        }
+
+        // Show loading indicator
+        useIsLoading(true);
+
+        // Function to fetch and set user's address details
+        const getAdddress = async () => {
+            // If user is not logged in, stop loading and return
+            if (!user?.id) {
+                useIsLoading(false);
+                return;
+            }
+
+            setIsLoadingAddress(true);
+            const response = await useUserAddress();
+            if (response) setAddressDetails(response);
+            setIsLoadingAddress(false);
+        };
+
+        // Fetch address details and initialize Stripe after a delay
+        getAdddress();
+        setTimeout(() => stripeInit(), 300);
+    }, [user]);
+
+    // Initialize Stripe elements and set client secret
+    const stripeInit = async () => {
+        stripe.current = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK_KEY || '');
+
+        // Fetch client secret from server
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            body: JSON.stringify({ amount: cart.cartTotal() })
+        });
+        const result = await response.json();
+
+        clientSecret.current = result.client_secret;
+        elements.current = stripe.current.elements();
+        var style = {
+            base: { fontSize: "18px" },
+            invalid: {
+                fontFamily: 'Arial, sans-serif',
+                color: "#EE4B2B",
+                iconColor: "#EE4B2B"
+            }
+        };
+        card.current = elements.current.create("card", { hidePostalCode: true, style: style });
+
+        card.current.mount("#card-element");
+        card.current.on("change", function (event) {
+            document.querySelector("button").disabled = event.empty;
+            document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+        });
+
+        // Stop loading indicator
+        useIsLoading(false);
+    };
+
+    // Handle payment submission
+    const pay = async (event) => {
+        event.preventDefault();
+
+        // If no address details, show error
+        if (Object.entries(addressDetails).length === 0) {
+            showError('Please add shipping address!');
+            return;
+        }
+
+        // Confirm payment with Stripe
+        let result = await stripe.current.confirmCardPayment(clientSecret.current, {
+            payment_method: { card: card.current },
+        });
+
+        // Handle payment result
+        if (result.error) {
+            showError(result.error.message);
+        } else {
+            useIsLoading(true);
+
+            try {
+                // Create order on the server
+                let response = await fetch('/api/orders/create', {
+                    method: "POST",
+                    body: JSON.stringify({
+                        stripe_id: result.paymentIntent.id,
+                        name: addressDetails.name,
+                        address: addressDetails.address,
+                        zipcode: addressDetails.zipcode,
+                        city: addressDetails.city,
+                        country: addressDetails.country,
+                        products: cart.getCart(),
+                        total: cart.cartTotal()
+                    })
+                });
+
+                // If order created successfully, show success message and clear cart
+                if (response.status === 200) {
+                    toast.success('Order Complete!', { autoClose: 3000 });
+                    cart.clearCart();
+                    return router.push('/success');
+                }
+            } catch (error) {
+                console.log(error);
+                toast.error('Something went wrong?', { autoClose: 3000 });
+            }
+
+            // Stop loading indicator
+            useIsLoading(false);
+        }
+    };
+
+    // Show error message
+    const showError = (errorMsgText) => {
+        let errorMsg = document.querySelector("#card-error");
+        toast.error(errorMsgText, { autoClose: 3000 });
+        errorMsg.textContent = errorMsgText;
+        setTimeout(() => { errorMsg.textContent = "" }, 3000);
+    };
+
+    return (
         <>
-           <MainLayout>
-             <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
-                <div className="mt-4 mb-4 text-2xl font-bold">
-                    Checkout
-                </div>
-                <div className="relative flex items-baseline justify-between w-full gap-4 mx-auto">
-                    <div className="w-[65%]">
-                        <div className="p-4 bg-white border rounded-lg">
+            <MainLayout>
+                <div id="CheckoutPage" className="mt-4 max-w-[1100px] mx-auto">
 
-                            <div className="mb-2 text-xl font-semibold">Shipping Address</div>
-                            <div>
-                                <ul className="mt-2 text-sm">
-                                    <li>Name: test</li>
-                                    <li>Address: test</li>
-                                    <li>Zipcode: test</li>
-                                    <li>City: test</li>
-                                    <li>Country: test</li> 
-                                </ul>
-                            </div>
-                        </div>
-                        <div id="Items" className="mt-4 bg-white rounded-lg">
-                        <CheckoutItem key={product.id} product={product}/>
-                        </div>
-                    </div>
+                    <div className="mt-4 mb-4 text-2xl font-bold">Checkout</div>
 
-                    <div id="PlaceOrder" className="relative -top-[6px] w-[35%] border rounded-lg">
-                        <div className="p-4">
-                            <div className="flex items-baseline justify-between mb-1 text-sm">
-                                <div>Items (3)</div>
-                                <div>£12.99</div>
-                            </div>
-                            <div className="flex items-center justify-between mb-4 text-sm">
-                                <div>Shipping:</div>
-                                <div>Free</div>
-                            </div>
+                    <div className="relative flex items-baseline justify-between w-full gap-4 mx-auto">
+                        <div className="w-[65%]">
+                            <div className="p-4 bg-white border rounded-lg">
 
-                            <div className="border-t"/>
+                                <div className="mb-2 text-xl font-semibold">Shipping Address</div>
 
-                            <div className="flex items-center justify-between my-4">
-                                <div className="font-semibold">Order total</div>
-                                <div className="text-2xl font-semibold">
-                                   £12.99  
+                                <div>
+                                    {!isLoadingAddress ?
+                                        <Link href="/address" className="text-sm text-blue-500 underline">
+                                            {addressDetails.name ? 'Update Address' : 'Add Address'}
+                                        </Link>
+                                        : null}
+
+                                    {!isLoadingAddress && addressDetails.name ?
+                                        <ul className="mt-2 text-sm">
+                                            <li>Name: {addressDetails.name}</li>
+                                            <li>Address: {addressDetails.address}</li>
+                                            <li>Zip: {addressDetails.zipcode}</li>
+                                            <li>City: {addressDetails.city}</li>
+                                            <li>Country: {addressDetails.country}</li>
+                                        </ul>
+                                        : null}
+
+                                    {isLoadingAddress ?
+                                        <div className="flex items-center gap-2 mt-1">
+                                            <AiOutlineLoading3Quarters className="animate-spin" />
+                                            Getting Address...
+                                        </div>
+                                        : <div></div>}
                                 </div>
                             </div>
-                            
-                            <form>
-                                <div className="p-2 border border-gray-500 rounded-sm" id="card-element"/>
 
-                                <p id="card-error"
-                                   role="alert"
-                                   className="relative font-semibold text-center text-red-700 top-2"
-                                />
-
-                                <button type="submit" className="w-full p-3 mt-4 text-lg font-semibold text-white bg-blue-600 rounded-full">
-                                    Confirm and pay
-                                </button> 
-                            </form>     
+                            <ClientOnly>
+                                <div id="Items" className="mt-4 bg-white rounded-lg">
+                                    {cart.getCart().map(product => (
+                                        <CheckoutItem key={product.id} product={product} />
+                                    ))}
+                                </div>
+                            </ClientOnly>
                         </div>
-                        <div className="flex items-center justify-center gap-2 p-4 border-t">
-                            <img width={50} src="/images/logo.svg"/>
-                            <div className="mt-2 mb-2 font-light">MONEY BACK GUARANTEE</div>
+
+                        <div id="PlaceOrder" className="relative -top-[6px] w-[35%] border rounded-lg">
+                            <ClientOnly>
+                                <div className="p-4">
+                                    <div className="flex items-baseline justify-between mb-1 text-sm">
+                                        <div>Items ({cart.getCart().length})</div>
+                                        <div>£{(cart.cartTotal() / 100).toFixed(2)}</div>
+                                    </div>
+                                    <div className="flex items-center justify-between mb-4 text-sm">
+                                        <div>Shipping:</div>
+                                        <div>Free</div>
+                                    </div>
+
+                                    <div className="border-t" />
+
+                                    <div className="flex items-center justify-between my-4">
+                                        <div className="font-semibold">Order total</div>
+                                        <div className="text-2xl font-semibold">
+                                            £{(cart.cartTotal() / 100).toFixed(2)}
+                                        </div>
+                                    </div>
+
+                                    <form onSubmit={pay}>
+                                        <div
+                                            className="p-2 border border-gray-500 rounded-sm"
+                                            id="card-element"
+                                        />
+
+                                        <p
+                                            id="card-error"
+                                            role="alert"
+                                            className="relative font-semibold text-center text-red-700 top-2"
+                                        />
+
+                                        <button
+                                            type="submit"
+                                            className="w-full p-3 mt-4 text-lg font-semibold text-white bg-blue-600 rounded-full"
+                                        >
+                                            <div>Confirm and pay</div>
+                                        </button>
+                                    </form>
+                                </div>
+                            </ClientOnly>
+
+                            <div className="flex items-center justify-center gap-2 p-4 border-t">
+                                <img width={50} src="/images/logo.svg" />
+                                <div className="mt-2 mb-2 font-light">MONEY BACK GUARANTEE</div>
+                            </div>
                         </div>
                     </div>
-                    
                 </div>
-
-             </div>
-           </MainLayout>
+            </MainLayout>
         </>
-    )
+    );
 }
