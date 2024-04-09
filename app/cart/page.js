ce90ecb9b69b7f00c549c70f09f812fd7443f6f6
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-09 16:12:41 +0300

    Feat: Finishing the cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,58 +1,83 @@
 "use client"
 
+// Client-only imports to prevent Next.js hydration errors
 import MainLayout from "../layouts/MainLayout"
 import SimilarProducts from "../components/SimilarProducts"
 import CartItem from "../components/CartItem"
+import { useCart } from "../context/cart"
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+import useIsLoading from "../hooks/useIsLoading"
+import ClientOnly from "../components/ClientOnly"
+
 export default function Cart() {
+  const router = useRouter()
+  const cart = useCart()
 
-    const products = {
-        id: 2,
-        title: "School Books",
-        description: "The school book is a guide for students, offering clear explanations and information on various subjects to support their learning.",
-        url: "https://picsum.photos/id/20",
-        price: 1999
+  // useEffect to fetch cart data and calculate cart total
+  useEffect(() => { 
+    useIsLoading(true)
+    cart.getCart() 
+    cart.cartTotal()
+    useIsLoading(false)
+  }, [cart])
+
+  // Function to navigate to checkout page
+  const goToCheckout = () => {
+    if (!cart.cartTotal()) {
+      alert("You don't have any items in the cart.")
+      return
     }
+    router.push('/checkout')
+  }
 
-    return(
-        <>
-           <MainLayout>
-            <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]">
-                <div className="my-4 text-2xl font-bold">Shopping Cart </div>
-                <div className="relative flex items-baseline justify-between gap-2">
-                    <div className="w-[65%]">
-                        <CartItem key={products.id} product={products}/>
-                    </div>
-                    <div id="GoToCheckout" className="md:w-[33%] absolute top-0 right-0 m-2">
-                        <div className="p-4 bg-white border">
-                            
-                            <button className="flex items-center justify-center w-full p-3 mt-4 font-semibold text-white bg-blue-600 rounded-full">
-                                Go to Checkout
-                            </button>
+  return (
+    <>
+      <MainLayout>
+        <div className="max-w-[1200px] mx-auto mb-8 min-h-[300px]">
+          <div className="my-4 text-2xl font-bold">Shopping cart</div>
+          <div className="relative flex items-baseline justify-between gap-2">
+            <ClientOnly>
+              <div className="w-[65%]">
+                {cart.getCart().map(product => (
+                  <CartItem key={product.id} product={product}/>
+                ))}
+              </div>
+            </ClientOnly>
 
-                            <div className="flex items-center justify-between mt-4 mb-1 text-sm">
-                                <div>Items (3)</div>
-                                <div>£12.99</div>
-                            </div>
-                            <div className="flex items-center justify-between mt-4 text-sm">
-                                <div>Shipping</div>
-                                <div>Free</div>
-                            </div>
+            <div id="GoToCheckout" className="md:w-[33%] absolute top-0 right-0 m-2">
+              <ClientOnly>
+                <div className="p-4 bg-white border">
+                  <button 
+                    onClick={() => goToCheckout()} 
+                    className="flex items-center justify-center w-full p-3 mt-4 font-semibold text-white bg-blue-600 rounded-full"
+                  >
+                    Go to checkout
+                  </button>
 
-                            <div className="border-b border-gray-300"/>
+                  <div className="flex items-center justify-between mt-4 mb-1 text-sm">
+                    <div>Items ({cart.getCart().length})</div>
+                    <div>£{(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
+                  <div className="flex items-center justify-between mb-4 text-sm">
+                    <div>Shipping:</div>
+                    <div>Free</div>
+                  </div>
 
-                            <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
-                                <div>Subtotal</div>
-                                <div>£12.99</div>
-                            </div>
-                            
-                        </div>
-                        
-                    </div>
-                    
+                  <div className="border-b border-gray-300"/>
+
+                  <div className="flex items-center justify-between mt-4 mb-1 text-lg font-semibold">
+                    <div>Subtotal</div>
+                    <div>£{(cart.cartTotal() / 100).toFixed(2)}</div>
+                  </div>
                 </div>
+              </ClientOnly>
             </div>
-              <SimilarProducts />
-           </MainLayout>
-        </>
-    )
+          </div>
+        </div>
+
+        <SimilarProducts />
+      </MainLayout>
+    </>
+  )
 }
--- app/components/CartItem.js
+++ app/components/CartItem.js
@@ -1,38 +1,51 @@
 "use client"
 
+// Client-only imports to prevent Next.js hydration errors
+import { useCart } from "../context/cart"
+import { toast } from "react-toastify"
 
-export default function CartItem({ product }){
-    return (
-        <>
-          <div className="relative flex justify-start w-full p-6 my-2 border">
-             <img src={product?.url+'/150'} className="rounded-md w-[150px] h-[150px]"/>
+export default function CartItem({ product }) {
+  const cart = useCart()
 
-             <div className="w-full pl-2 overflow-hidden">
-                <div className="flex items-center justify-between w-full">
-                    <div className="flex items-center justify-between font-semibold w-[400px] text-[16px] underline">
-                        {product?.title}
-                    </div>
-                    <div className="text-lg font-bold">
-                        £{(product?.price / 100).toFixed(2)}      
-                    </div>  
-                </div>
+  // Function to remove item from cart
+  const removeItemFromCart = () => {
+    let res = confirm(`Are you sure you want to remove this? "${product.title}"`)
+    if (res) {
+        cart.removeFromCart(product)
+        toast.info('Removed from cart', { autoClose: 3000 })
+    }
+  }
 
-                <div className="mt-2 font-semibold">
-                    NEW
-                </div>
-                <div className="mt-2 text-sm">
-                    {product?.description.substring(0, 150)}...
-                </div>
+  return (
+    <>
+      <div className="relative flex justify-start w-full p-6 my-2 border">
+        <img src={product?.url+'/150'} className="rounded-md w-[150px] h-[150px]" />
 
-                <div className="absolute bottom-0 right-0 p-4 text-sm">
-                    <button className="text-blue-500 underline">
-                        Remove
-                    </button>
-                    
-                </div>
-                     
-             </div>
+        <div className="w-full pl-2 overflow-hidden">
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-center font-semibold justify-between w-[400px] text-[16px] underline">
+              {product?.title}
+            </div>
+            <div className="text-lg font-bold">
+              £{(product?.price / 100).toFixed(2)}
+            </div>
           </div>
-        </>
-    )
+
+          <div className="mt-2 font-semibold">
+            NEW
+          </div>
+
+          <div className="mt-2 text-sm">
+            {product?.description.substring(0, 150)}...
+          </div>
+
+          <div className="absolute bottom-0 right-0 p-4 text-sm">
+            <button onClick={() => removeItemFromCart()} className="text-blue-500 underline">
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
